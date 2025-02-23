ee0b13062c5c2e29a5069c2032e5fe8fe4c96855
johnstone78-arch <qwerty91888@outlook.com>
2025-02-23 13:17:14 +1000

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -37,8 +37,8 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            addedTokens: 10000,
+                            addedXp: 3000
                         }, e.secret)
                     });
                     fetch("https://api.blooket.com/api/users/add-rewards", {
